6bac664257efa5cf0767856ca47b290631ecd442
firebear <firebear.business@gmail.com>
2020-04-05 03:16:16 +0000

    add bootstrap design

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(status: '未着手', content: '乃木坂46　全国握手会')
+Task.create(status: '未着手', content: '欅坂46　全国握手会')
+Task.create(status: '未着手', content: '日向坂46　全国握手会')
+Task.create(status: '未着手', content: '乃木坂46　個別握手会')
+Task.create(status: '未着手', content: '欅坂46　個別握手会')
+Task.create(status: '未着手', content: '日向坂46　個別握手会')
